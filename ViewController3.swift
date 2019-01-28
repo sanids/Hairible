07076e1ca9ef8fb94ceca41b95c76f0dae322cc8
Sanid Singhal <sanid64@gmail.com>
2019-01-28 11:47:41 -0800

    NWhacks2019

---
+++ ViewController3.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController3.swift
+//  Hairable
+//
+//  Created by Sanid Singhal on 2019-01-27.
+//  Copyright © 2019 Sanid Singhal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController3: UIViewController {
+    
+    
+        
+        let gradientLayer = CAGradientLayer()
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+        }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
